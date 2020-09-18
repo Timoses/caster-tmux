8addbd57b6339d1c6703b51d47fac6d1df33014c
Timo Funke <timo.funke@biotronik.com>
2020-09-18 20:20:44 +0200

    Implement control plugin

---
+++ castercontrol/__init__.py
@@ -0,0 +1,70 @@
+from dragonfly import MappingRule, Function, Grammar
+
+from mycastervoice import Plugin
+from mycastervoice.core import get_controller
+
+_plugin_name = None
+_plugins_are_asleep = False
+_sleeping_plugins = []
+
+def sleep():
+    global _plugin_name
+    global _plugins_are_asleep
+    global _sleeping_plugins
+
+    if _plugins_are_asleep:
+        return
+
+    assert(len(_sleeping_plugins) == 0) # Ups, did I forget to wake someone?
+    for plugin in get_controller().plugin_manager.plugins:
+        if plugin.name != _plugin_name:
+            _sleeping_plugins.append(plugin)
+            plugin.disable()
+
+    _plugins_are_asleep = True
+
+
+def wake():
+    global _plugin_name
+    global _plugins_are_asleep
+    global _sleeping_plugins
+
+    if not _plugins_are_asleep:
+        assert(len(_sleeping_plugins) == 0) # You shouldn't be asleep!
+
+    while len(_sleeping_plugins):
+        plugin = _sleeping_plugins[0]
+        if plugin.name != _plugin_name:
+            plugin.enable()
+            _sleeping_plugins.remove(plugin)
+
+    _plugins_are_asleep = False
+
+class ControlRule(MappingRule):
+
+    mapping = {
+        "bye mike":
+            Function(sleep),
+        "hi mike":
+            Function(wake)
+    }
+    extras = [
+    ]
+    defaults = {}
+
+
+class ControlPlugin(Plugin):
+
+    """Docstring for DictationPlugin. """
+
+    def __init__(self, name):
+        """TODO: to be defined. """
+
+        global _plugin_name
+        _plugin_name = name
+        super().__init__(name)
+
+    def get_grammars(self):
+        grammar = Grammar(name="Control")
+        grammar.add_rule(ControlRule())
+        return [grammar]
